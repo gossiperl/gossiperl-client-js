f3980663441293d854c5599efd7eb50c8c5fabcd
radekg <radek@gruchalski.com>
2015-01-12 21:31:16 +0100

    General process working in Node.JS. Fixes #1.

--- test/tests.node.js
+++ test/tests.node.js
@@ -11,10 +11,51 @@ var overlayPort = 6666;
 var events = ["member_in", "digestForwardableTest"];
 
 module.exports = testCase({
+
   "Gossiperl context": function(test) {
     test.equal( Gossiperl.CurrentContext, Gossiperl.Context.NODEJS );
     test.done();
   },
+
+  "Process": function(test) {
+    var supervisor = new Gossiperl.Client.Supervisor();
+    test.equal(supervisor.getNumberOfConnections(),0);
+    var config = {
+      overlayName:  overlayName,
+      overlayPort:  overlayPort,
+      clientName:   clientName,
+      clientPort:   clientPort,
+      clientSecret: clientSecret,
+      symmetricKey: symmetricKey
+    };
+    supervisor.connect(config);
+    test.equal(supervisor.getNumberOfConnections(),1);
+    
+    setTimeout(function() {
+      
+      var resp = supervisor.subscribe( config.overlayName, events );
+      test.deepEqual(resp, events);
+
+      setTimeout(function() {
+        
+        var resp = supervisor.unsubscribe( config.overlayName, events );
+        test.deepEqual(resp, []);
+
+        setTimeout(function() {
+
+          supervisor.disconnect( config.overlayName );
+
+          setTimeout(function() {
+            
+            test.equal(supervisor.getNumberOfConnections(),0);
+            test.done();
+
+          }, 1500);
+        }, 3000);
+      }, 3000);
+    }, 3000);
+  },
+
   "Simple serialize / deserialize": function(test) {
     var digest = Gossiperl.Client.getAnnotatedDigest("Digest", {
       name: clientName,
@@ -32,6 +73,7 @@ module.exports = testCase({
     test.equal( deserialized.secret, clientSecret );
     test.done();
   },
+
   "Serialize / deserialize with encryption": function(test) {
     var aes = new Gossiperl.Client.Encryption.Aes256( symmetricKey );
     var digest = Gossiperl.Client.getAnnotatedDigest("Digest", {
@@ -53,4 +95,5 @@ module.exports = testCase({
     test.equal( deserialized.secret, clientSecret );
     test.done();
   }
+
 });
